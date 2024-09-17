2d16fe7b19826b720a5b8e456f3b71f2c6db5738
Henrique <henrique.sagara@hotmail.com>
2024-09-17 10:45:16 -0400

    remove venv

--- backend/venv/lib/python3.12/site-packages/pipenv/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from pipenv.patched.pip._vendor.rich.console import Console
-from pipenv.patched.pip._vendor.rich.prompt import Confirm  # noqa
-
-logging.basicConfig(level=logging.INFO)
-console = Console(highlight=False)
-err = Console(stderr=True)
--- backend/venv/lib/python3.12/site-packages/pipenv/vendor/plette/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    "__version__",
-    "Lockfile", "Pipfile",
-]
-
-__version__ = '2.1.0'
-
-from .lockfiles import Lockfile
-from .pipfiles import Pipfile
