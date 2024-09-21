cf20ddfaf9ff7f1ed6a2b1320cc69092cf5ef20f
John Pierre Mendoza <johnpierremendoza@gmail.com>
2024-09-21 16:21:32 +0800

    Create dummy user routes protected by middleware

---
+++ backend/src/routes/user.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, Request
+
+from ..auth import auth_middleware
+
+# These routes are protected by auth_middleware
+router = APIRouter(dependencies=[Depends(auth_middleware)])
+
+# These are dummy user routes
+@router.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@router.get("/users/me", tags=["users"])
+async def read_user_me():
+    return {"username": "fakecurrentuser"}
+
+
+@router.get("/users/{username}", tags=["users"])
+async def read_user(username: str):
+    return {"username": username}
