6aec34d372e1cb1bb1eb8901f2340cc4d2afe777
John Pierre Mendoza <johnpierremendoza@gmail.com>
2024-09-21 16:22:31 +0800

    Clean and update server.py

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,52 +1,66 @@
-# ./src/server.py
-from fastapi import FastAPI, Request, HTTPException, status, Depends
+from fastapi import FastAPI, HTTPException, status, Depends
 from fastapi.responses import JSONResponse, RedirectResponse
 from datetime import datetime, timezone
-import requests
-import os
-
 from urllib.parse import urlencode
 from dotenv import load_dotenv
 from urllib.parse import urlencode
-import auth.auth as authenticate
+import requests
+import os
 
+from .auth import auth_middleware
+from .routes import user
+
+load_dotenv()
+
+COGNITO_CLIENT_ID = os.getenv("COGNITO_CLIENT_ID")
+COGNITO_DOMAIN = os.getenv("COGNITO_DOMAIN")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
 
 app = FastAPI()
 
-load_dotenv()
-COGNITO_REGION = os.getenv("COGNITO_REGION")
-COGNITO_USERPOOL_ID = os.getenv("COGNITO_USERPOOL_ID")
-COGNITO_CLIENT_ID = os.getenv("COGNITO_CLIENT_ID")
-COGNITO_DOMAIN = os.getenv("COGNITO_DOMAIN")
-COGNITO_ISSUER = os.getenv("COGNITO_ISSUER")
-JWKS_URL = f"{COGNITO_ISSUER}/.well-known/jwks.json"
-REDIRECT_URI = os.getenv("REDIRECT_URI")
+# include user routes
+app.include_router(user.router)
 
+# Public Route Example (no authentication required)
+@app.get("/public")
+async def public_route():
+    return {"message": "This is a public route. No authentication needed."}
 
-# Custom Middleware for Authentication
-@app.middleware("http")
-async def auth_middleware(request: Request, call_next):
-    # Define routes that don't require authentication (e.g., public routes)
-    if request.url.path not in ["/public", "/login","/healthcheck", "/"]:
-        # Get the Authorization header from the request
-        auth_header = request.headers.get("Authorization")
-        if not auth_header:
-            return JSONResponse(status_code=401, content={"detail": "Authorization header missing"})
+# Protected Route Example
+@app.get("/protected", dependencies=[Depends(auth_middleware)])
+async def protected_route():
+    return {"message": "This is a protected route. Access granted."}
 
-        # Extract the token from the Authorization header ("Bearer <token>")
-        try:
-            token = auth_header.split(" ")[1]
-            authenticate.verify_jwt_token(token)
-        except Exception as e:
-            return JSONResponse(status_code=401, content={"detail": "Invalid token"})
+# Health check
+# Note: revert back to "/" once frontend login has been implemented
+@app.get("/healthcheck", status_code=200)
+async def root():
+    return JSONResponse(
+        content={
+            "status": "healthy",
+            "app": "WordVision Server",
+            "timestamp": datetime.now(timezone.utc).isoformat()
+        }
+    )
 
-    # Proceed to the next middleware or route handler if the token is valid
-    response = await call_next(request)
-    return response
+# Login
+# Note: delete this once frontend login has been implemented
+@app.get("/login")
+async def login():
+    # Construct the Cognito login URL with the necessary parameters
+    params = {
+        "client_id": COGNITO_CLIENT_ID,
+        "response_type": "code",  # Authorization Code Flow
+        "scope": "openid",  # Requesting the ID token
+        "redirect_uri": REDIRECT_URI
+    }
+    cognito_login_url = f"https://{COGNITO_DOMAIN}/login?{urlencode(params)}"
+    return RedirectResponse(url=cognito_login_url)
 
-# Root
-@app.get("/")  # Now handle the callback at the root path "/"
-async def callback(code: str = None):
+# Root path for handling login callback
+# Note: delete this once frontend login has been implemented
+@app.get("/")
+async def callback(code: str|None = None):
     if code is None:
         raise HTTPException(status_code=400, detail="Authorization code not found in request")
 
@@ -70,39 +84,3 @@ async def callback(code: str = None):
         return {"id_token": id_token, "access_token": access_token, "expires_in": expires_in}  # Return both tokens
 
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to exchange code for tokens")
-
-
-# Public Route Example (no authentication required)
-@app.get("/public")
-async def public_route():
-    return {"message": "This is a public route. No authentication needed."}
-
-# Protected Route Example
-@app.get("/protected")
-async def protected_route():
-    return {"message": "This is a protected route. Access granted."}
-
-# Health check at root "/"
-@app.get("/healthcheck", status_code=200)
-async def root():
-    return JSONResponse(
-        content={
-            "status": "healthy",
-            "app": "WordVision Server",
-            "timestamp": datetime.now(timezone.utc).isoformat()
-        }
-    )
-
-# Login
-@app.get("/login")
-async def login():
-    # Construct the Cognito login URL with the necessary parameters
-    params = {
-        "client_id": COGNITO_CLIENT_ID,
-        "response_type": "code",  # Authorization Code Flow
-        "scope": "openid",  # Requesting the ID token
-        "redirect_uri": REDIRECT_URI
-    }
-    cognito_login_url = f"https://{COGNITO_DOMAIN}/login?{urlencode(params)}"
-    return RedirectResponse(url=cognito_login_url)
-
