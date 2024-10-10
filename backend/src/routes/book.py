88043924123601143eb2bdf24d38f715cd5aa460
Henrique <henrique.sagara@hotmail.com>
2024-10-10 18:22:24 -0400

    saves to s3, but still have 500 error

--- backend/src/routes/book.py
+++ backend/src/routes/book.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, HTTPException, File, UploadFile
+# src/routes/book.py
+from fastapi import APIRouter, HTTPException, File, UploadFile, Form
 from fastapi.responses import JSONResponse
+from models.book import hash_email
+from models.book import Book
 from database.book_metadata import process_epub
 from database.s3_db import write_file_data
-from models.book import hash_email
+from io import BytesIO
 import uuid
 
 router = APIRouter()
@@ -16,12 +19,15 @@ async def upload_epub(file: UploadFile = File(...)):
         # Read file content
         file_content = await file.read()
 
+        # Convert bytes to a file-like object
+        file_stream = BytesIO(file_content)
+
         # Generate fileId and ownerId (example, you can modify as per your need)
         file_id = str(uuid.uuid4())
         owner_id = hash_email("wordvision.app@gmail.com")
 
         # Write file data to S3
-        upload_success = write_file_data(file_id, owner_id, file_content)
+        upload_success = write_file_data(file_id, owner_id, file_stream)
         if not upload_success:
             raise HTTPException(status_code=500, detail="Failed to upload file to S3")
         else:
@@ -31,6 +37,9 @@ async def upload_epub(file: UploadFile = File(...)):
         file_location = f"/app/uploads/{file.filename}"
         with open(file_location, "wb") as f:
             f.write(file_content)
+
+        # Since process_epub may use seek, reset file stream to the beginning
+        file_stream.seek(0)
 
         # Process the file (call process_epub for instance)
         process_epub(file_location)
